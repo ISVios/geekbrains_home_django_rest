076472cf1c89fba4fb7d392ae82af8c186269c4f
Vios <i.svoboda.vios@gmail.com>
2022-10-20 20:53:30 +0300

    make serialization

--- author/views.py
+++ author/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from author import models as author_models
+from author import serializers as author_selial
 
-# Create your views here.
+class PersoneModelViewSet(ModelViewSet):
+    queryset = author_models.PersoneModel.objects.all()
+    serializer_class = author_selial.PersoneModelSerializer
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from author import views as author_views
+
+router = DefaultRouter()
+router.register("author", author_views.PersoneModelViewSet, "PersoneModel")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
 ]
