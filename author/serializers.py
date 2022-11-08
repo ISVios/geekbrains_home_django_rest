ca8fc4b3d7bfeb2fb165919147896c88665bfc0d
vios <i.svoboda.vios@gmail.com>
2022-11-08 23:13:46 +0300

    fix to correct ser class extends

--- author/serializers.py
+++ author/serializers.py
@@ -1,19 +1,19 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import ModelSerializer
 from author import models as author_models
 
-class PersoneModelSerializer(HyperlinkedModelSerializer):
+class PersoneModelSerializer(ModelSerializer):
     class Meta:
         model = author_models.PersoneModel
         fields = ["username", "first_name", "surname", "email"]
 ###############################################################################
 
-class ProjectModelSerilizer(HyperlinkedModelSerializer):
+class ProjectModelSerilizer(ModelSerializer):
     class Meta:
         model = author_models.ProjectModel
         fields = ["name", "repo_url", "persones"]
 ###############################################################################
 
-class TodoModelSerilizer(HyperlinkedModelSerializer):
+class TodoModelSerilizer(ModelSerializer):
     class Meta:
         model = author_models.TodoModel
         fields = ["content", "project", "persone"]
