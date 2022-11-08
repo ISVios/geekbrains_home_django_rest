0478354b18855be505624b0b4a9a4d2fa34602ab
vios <i.svoboda.vios@gmail.com>
2022-11-08 22:53:52 +0300

    add view to project model

--- author/views.py
+++ author/views.py
@@ -5,3 +5,9 @@ from author import serializers as author_selial
 class PersoneModelViewSet(ModelViewSet):
     queryset = author_models.PersoneModel.objects.all()
     serializer_class = author_selial.PersoneModelSerializer
+###############################################################################
+
+class ProjectModelViewSet(ModelViewSet):
+    queryset = author_models.ProjectModel.objects.all()
+    serializer_class = author_selial.ProjectModelSerilizer
+###############################################################################
