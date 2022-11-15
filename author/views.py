2e92b002d2e09e080a655eb97432fb43e11cc2c6
vios <i.svoboda.vios@gmail.com>
2022-11-15 13:47:03 +0300

    add 100 size paginator

--- author/views.py
+++ author/views.py
@@ -1,23 +1,39 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from author import models as author_models
 from author import serializers as author_selial
 
-from djangorestframework_camel_case.render import CamelCaseJSONRenderer
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer,BrowsableAPIRenderer
+from rest_framework.pagination import LimitOffsetPagination
 
-class PersoneModelViewSet(ModelViewSet):
-    renderer_class = CamelCaseJSONRenderer
+class PersoneModelViewSet(mixins.ListModelMixin, 
+                          mixins.RetrieveModelMixin, 
+                          GenericViewSet):
+    renderer_classes = [CamelCaseJSONRenderer, BrowsableAPIRenderer]
     queryset = author_models.PersoneModel.objects.all()
     serializer_class = author_selial.PersoneModelSerializer
 ###############################################################################
 
 class ProjectModelViewSet(ModelViewSet):
+
+    class Pagger(LimitOffsetPagination):
+        default_limit = 10
+
     renderer_class = CamelCaseJSONRenderer
     queryset = author_models.ProjectModel.objects.all()
     serializer_class = author_selial.ProjectModelSerilizer
+    pagination_class = Pagger
 ###############################################################################
 
 class TodoModelViewSet(ModelViewSet):
+
+    class Pagger(LimitOffsetPagination):
+        default_limit = 20
+
     renderer_class = CamelCaseJSONRenderer
     queryset = author_models.TodoModel.objects.all()
     serializer_class = author_selial.TodoModelSerilizer
+    pagination_class = Pagger
 ###############################################################################
--- config/settings.py
+++ config/settings.py
@@ -135,6 +135,9 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
