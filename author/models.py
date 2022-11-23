f06628e64087d280afbefd167afdf1b5cafc887a
vios <i.svoboda.vios@gmail.com>
2022-11-23 17:32:58 +0300

    fix broken admin

--- author/models.py
+++ author/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.mail import send_mail
 from django.utils.translation import gettext_lazy as _
@@ -21,6 +21,8 @@ class PersoneModel(AbstractBaseUser, PermissionsMixin):
     django model for validators
     """
     username_validator = ASCIIUsernameValidator()
+    
+    objects = UserManager()
 
     username = CharFieldCaseIgnore(
         _("username"),
@@ -67,6 +69,13 @@ class PersoneModel(AbstractBaseUser, PermissionsMixin):
         auto_now_add=True,
         editable=False
     )
+
+    is_staff = models.BooleanField(
+            default=False
+            )
+    is_active = models.BooleanField(
+            default=False
+            )
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
     'author',
     'django_filters'
 ]
@@ -152,6 +153,11 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.parser.CamelCaseJSONParser',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-        ]
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
