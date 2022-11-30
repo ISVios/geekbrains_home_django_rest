e662dbb9dc1d3b28bae07d3b63ffcbee3b5d0fca
vios <i.svoboda.vios@gmail.com>
2022-11-30 18:50:39 +0300

    add imports

--- author/tests.py
+++ author/tests.py
@@ -1,3 +1,16 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import (
+    APIRequestFactory,
+    force_authenticate,
+    APIClient,
+    APISimpleTestCase,
+    APITestCase,
+)
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from author import views as author_views
+from author import models as author_models
 
 # Create your tests here.
