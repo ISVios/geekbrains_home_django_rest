64e2a1e4fa3c3dc5361cb00f11f64be10d9fefaf
vios <i.svoboda.vios@gmail.com>
2022-12-01 22:24:13 +0300

    add api version

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
@@ -28,10 +28,11 @@ router.register("project", author_views.ProjectModelViewSet, "ProjectModel")
 filter_router = DefaultRouter()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('api/', include(router.urls)),
-    path('api/todo/', author_views.TodoModelAPIView.as_view()),
-    path('api/todo/<int:pk>', author_views.TodoModelAPIView.as_view()),
-    path('api-token-auth/', views.obtain_auth_token)
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    re_path(r"^api/(?P<version>v\d)\/", include(router.urls)),
+    re_path("api/", include(router.urls)),
+    path("api/todo/", author_views.TodoModelAPIView.as_view()),
+    path("api/todo/<int:pk>", author_views.TodoModelAPIView.as_view()),
+    path("api-token-auth/", views.obtain_auth_token),
 ]
