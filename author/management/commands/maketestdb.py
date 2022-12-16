dde17befc8d57ba9dc7e23a1d61a7f867924c112
vios <i.svoboda.vios@gmail.com>
2022-12-17 01:09:10 +0300

    maketestdb can run with -y --yes param

--- author/management/commands/maketestdb.py
+++ author/management/commands/maketestdb.py
@@ -3,27 +3,48 @@ from django.core.management import call_command
 
 from author import models as author_models
 
+
 class Command(BaseCommand):
     help = "full ths database a test data"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "-y",
+            "--yes",
+            action="store_true",
+        )
+
+    def drop_tables_data(self):
+        for table in [
+            author_models.TodoModel,
+            author_models.ProjectModel,
+            author_models.PersoneModel,
+        ]:
+            table.objects.all().delete()
+
+    def full_database(self):
+        for fixture in ["0001_persone", "0002_project", "0003_todo"]:
+            call_command("loaddata", fixture)
 
     def handle(self, *args, **options) -> None:
         try:
-            self.stdout.write("WARNING: this command delete all" +
-                              " date from persone project todot tabbles")
+            answer = "no"
+            yes: bool = options["yes"]
+
+            self.stdout.write(
+                "WARNING: this command delete all"
+                + " date from persone project todot tabbles"
+            )
             self.stdout.write("print YES if you sure.")
-            answer = input("Sure(default no): ")
+            if yes:
+                answer = "YES"
+            else:
+                answer = input("Sure(default no): ")
+
             if answer == "YES":
                 self.stdout.write("delete all date from tables")
-
-                for table in [
-                        author_models.TodoModel, 
-                        author_models.ProjectModel, 
-                        author_models.PersoneModel]:
-                    table.objects.all().delete()
-                call_command("loaddata", "0001_persone")
-                call_command("loaddata", "0002_project")
-                call_command("loaddata", "0003_todo")
+                self.drop_tables_data()
+                self.full_database()
             elif answer == "" or answer.lower() == "no":
                 self.stdout.write("OK")
         except CommandError as djerror:
